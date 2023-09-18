19c7438a7f94d2931e0b66f8a73dd71656c2f1d3
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2023-09-18 07:20:28 -0600

    Prepare for Java 21

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,4 @@
 module whats.newjava {
-    requires jdk.incubator.concurrent;
     requires org.reactivestreams;
     requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.core;
