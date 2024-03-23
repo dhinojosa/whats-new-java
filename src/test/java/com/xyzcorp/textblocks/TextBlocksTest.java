ed818c7ede9bbc67949285131049ac0c74fa9200
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2024-03-23 09:54:53 -0600

    Changed what a wonderful world example to do a count.

--- src/test/java/com/xyzcorp/textblocks/TextBlocksTest.java
+++ src/test/java/com/xyzcorp/textblocks/TextBlocksTest.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -48,20 +49,24 @@ public class TextBlocksTest {
     @Test
     void testTextBlockStandard() {
         var lyrics = """
-            I see trees of green,
-            red roses too,
+            I see trees of green
+            red roses too
             I see them bloom
             For me and you
             And I think to myself
             What a wonderful world""";
 
-        Map<Character, List<String>> map =
-            Arrays.stream(lyrics.split("\n"))
-                  .flatMap(s -> Arrays.stream(s.split(" ")))
-                  .collect(Collectors.groupingBy(s -> Character.toLowerCase(s.charAt(0))));
+        Map<Character, Integer> result = Arrays.stream(lyrics.split("\n"))
+            .map(String::trim)
+            .flatMap(s -> Arrays.stream(s.split("\\s")))
+            .collect(Collectors.groupingBy(s -> Character.toLowerCase(s.charAt(0))))
+            .entrySet()
+            .stream()
+            .collect(Collectors.toMap(Map.Entry::getKey,
+                characterListEntry -> characterListEntry.getValue().size()));
 
-        assertThat(map).containsValues(List.of("red", "roses"));
-        System.out.println(map);
+        assertThat(result).isNotEmpty();
+        assertThat(result.get('t')).isEqualTo(5);
     }
 
     @Test
@@ -76,7 +81,7 @@ public class TextBlocksTest {
     void textBlocksCannotBeStartedOnTheSameLineAsTripleQuotes() {
         var tripleQuoteError = """
             I will start here
-              but I will have an error""";
+            but I will have an error""";
         System.out.println(tripleQuoteError);
     }
 
