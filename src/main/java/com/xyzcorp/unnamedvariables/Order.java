a18471980fec878474a4a10c172164e09339f84a
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2024-03-23 09:54:10 -0600

    Cleaned up examples for unnamed variables

--- src/main/java/com/xyzcorp/unnamedvariables/Order.java
+++ src/main/java/com/xyzcorp/unnamedvariables/Order.java
@@ -1,7 +1,5 @@
 package com.xyzcorp.unnamedvariables;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
---
+++ src/main/java/com/xyzcorp/unnamedvariables/SideEffects.java
@@ -0,0 +1,8 @@
+package com.xyzcorp.unnamedvariables;
+
+public class SideEffects {
+    static int sideEffect() {
+        System.out.println("The time is");
+        return 0;
+    }
+}
